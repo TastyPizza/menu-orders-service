cd2d0eee747d0294fac654c671159f3caafe5851
Vladislav Kharin <vladkharin19@gmail.com>
2023-04-09 15:41:55 +0300

    Initial commit

---
+++ src/main/kotlin/com/tastypizza/menuorders/MenuordersApplication.kt
@@ -0,0 +1,11 @@
+package com.tastypizza.menuorders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MenuordersApplication
+
+fun main(args: Array<String>) {
+    runApplication<MenuordersApplication>(*args)
+}
---
+++ src/test/kotlin/com/tastypizza/menuorders/MenuordersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tastypizza.menuorders
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MenuordersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
